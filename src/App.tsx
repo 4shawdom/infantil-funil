e4fef2cd261b21619f16cc3a661e03c2da97b476
4shawdom <4shawdompenido@gmail.com>
2026-04-28 22:21:50 -0300

    fix: scroll para o topo ao navegar entre páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Index from "./pages/Index";
 import Quiz from "./pages/Quiz";
 import Resultado from "./pages/Resultado";
 import Vendas from "./pages/Vendas";
 import Checkout from "./pages/Checkout";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/quiz" element={<Quiz />} />
